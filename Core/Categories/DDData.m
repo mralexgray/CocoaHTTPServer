874618e417e0340a4036bfc49eb77f63a20ecb02
Carter Allen <CarterA@opt-6.com>
2012-02-05 16:12:09 -0700

    Switched to a cleaner Base64 decoding implementation, without analyzer warnings.

--- Core/Categories/DDData.m
+++ Core/Categories/DDData.m
@@ -10,6 +10,8 @@ static char encodingTable[64] = {
 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
 'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/' };
 
+static int8_t kDecodingTable[256];
+
 - (NSData *)md5Digest
 {
 	unsigned char result[CC_MD5_DIGEST_LENGTH];
@@ -95,63 +97,35 @@ static char encodingTable[64] = {
 
 - (NSData *)base64Decoded
 {
-	const unsigned char	*bytes = [self bytes];
-	NSMutableData *result = [NSMutableData dataWithCapacity:[self length]];
-	
-	unsigned long ixtext = 0;
-	unsigned long lentext = [self length];
-	unsigned char ch = 0;
-	unsigned char inbuf[4], outbuf[3];
-	short i = 0, ixinbuf = 0;
-	BOOL flignore = NO;
-	BOOL flendtext = NO;
-	
-	while( YES )
-	{
-		if( ixtext >= lentext ) break;
-		ch = bytes[ixtext++];
-		flignore = NO;
+	const char *string = [self bytes];
+	NSUInteger inputLength = [self length];
+	NSUInteger outputLength = inputLength * 3 / 4;
+    NSMutableData* data = [NSMutableData dataWithLength:outputLength];
+    uint8_t* output = data.mutableBytes;
+    
+    NSUInteger inputPoint = 0;
+    NSUInteger outputPoint = 0;
+    while (inputPoint < inputLength) {
+        uint8_t i0 = string[inputPoint++];
+        uint8_t i1 = string[inputPoint++];
+        uint8_t i2 = inputPoint < inputLength ? string[inputPoint++] : 'A'; /* 'A' will decode to \0 */
+        uint8_t i3 = inputPoint < inputLength ? string[inputPoint++] : 'A';
+        
+        if (kDecodingTable[i0] < 0 || kDecodingTable[i1] < 0 
+			|| kDecodingTable[i2] < 0 || kDecodingTable[i3] < 0)
+            return nil;
 		
-		if( ( ch >= 'A' ) && ( ch <= 'Z' ) ) ch = ch - 'A';
-		else if( ( ch >= 'a' ) && ( ch <= 'z' ) ) ch = ch - 'a' + 26;
-		else if( ( ch >= '0' ) && ( ch <= '9' ) ) ch = ch - '0' + 52;
-		else if( ch == '+' ) ch = 62;
-		else if( ch == '=' ) flendtext = YES;
-		else if( ch == '/' ) ch = 63;
-		else flignore = YES;
-		
-		if( ! flignore )
-		{
-			short ctcharsinbuf = 3;
-			BOOL flbreak = NO;
-			
-			if( flendtext )
-			{
-				if( ! ixinbuf ) break;
-				if( ( ixinbuf == 1 ) || ( ixinbuf == 2 ) ) ctcharsinbuf = 1;
-				else ctcharsinbuf = 2;
-				ixinbuf = 3;
-				flbreak = YES;
-			}
-			
-			inbuf [ixinbuf++] = ch;
-			
-			if( ixinbuf == 4 )
-			{
-				ixinbuf = 0;
-				outbuf [0] = ( inbuf[0] << 2 ) | ( ( inbuf[1] & 0x30) >> 4 );
-				outbuf [1] = ( ( inbuf[1] & 0x0F ) << 4 ) | ( ( inbuf[2] & 0x3C ) >> 2 );
-				outbuf [2] = ( ( inbuf[2] & 0x03 ) << 6 ) | ( inbuf[3] & 0x3F );
-				
-				for( i = 0; i < ctcharsinbuf; i++ )
-					[result appendBytes:&outbuf[i] length:1];
-			}
-			
-			if( flbreak )  break;
-		}
-	}
+        output[outputPoint++] = (kDecodingTable[i0] << 2) | (kDecodingTable[i1] >> 4);
+        if (outputPoint < outputLength) {
+            output[outputPoint++] = ((kDecodingTable[i1] & 0xf) << 4) | (kDecodingTable[i2] >> 2);
+        }
+        if (outputPoint < outputLength) {
+            output[outputPoint++] = ((kDecodingTable[i2] & 0x3) << 6) | kDecodingTable[i3];
+        }
+    }
+    
+    return data;
 	
-	return [NSData dataWithData:result];
 }
 
 @end
